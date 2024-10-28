dcc49a2b648dd3f3ef7ea9ca7f0d3629467d6c2c
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-10-28 19:34:44 +0100

    Correction de l'erreur nil (joueur n'a pas de caractère en mode map)

--- control.lua
+++ control.lua
@@ -1,10 +1,12 @@
 script.on_event(defines.events.on_player_changed_position,
     function(event)
-        local player = game.get_player(event.player_index) -- get the player that moved            
-        -- if they're wearing our armor
-        if player and player.get_inventory(defines.inventory.character_armor).get_item_count("fire-armor") >= 1 then
-            -- create the fire where they're standing
-            player.surface.create_entity{name="fire-flame", position=player.position, force="neutral"} 
+        local player = game.get_player(event.player_index) -- get le joueur
+        if player and player.character then  -- Verifie si le joueur a un personnage (mode spectateur ou map)
+            local armor_inventory = player.get_inventory(defines.inventory.character_armor) -- récupération de l'armure
+            if armor_inventory and armor_inventory.get_item_count("fire-armor") >= 1 then
+                -- créé le feu où le joueur se trouve
+                player.surface.create_entity{name="fire-flame", position=player.position, force="neutral"} 
+            end
         end
     end
 )
